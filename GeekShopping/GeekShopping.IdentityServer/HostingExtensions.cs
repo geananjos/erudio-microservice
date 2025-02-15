84d6db554e6dd528ddf5065ac40ab106af49c239
Gean Anjos <gean.miguelf@gmail.com>
2025-02-15 12:49:08 -0300

    feat: add Duende Identity Server

---
+++ GeekShopping/GeekShopping.IdentityServer/HostingExtensions.cs
@@ -0,0 +1,63 @@
+using GeekShopping.IdentityServer.Model.Context;
+using GeekShopping.IdentityServer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+namespace GeekShopping.IdentityServer
+{
+    internal static class HostingExtensions
+    {
+        public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddRazorPages();
+
+            var connection = builder.Configuration["SqlConnection:SqlConnectionString"];
+            builder.Services.AddDbContext<SqlContext>(options =>
+                options.UseSqlServer(connection, sqlOptions =>
+                    sqlOptions.EnableRetryOnFailure()));
+
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<SqlContext>()
+                .AddDefaultTokenProviders();
+
+            builder.Services
+                .AddIdentityServer(options =>
+                {
+                    options.Events.RaiseErrorEvents = true;
+                    options.Events.RaiseInformationEvents = true;
+                    options.Events.RaiseFailureEvents = true;
+                    options.Events.RaiseSuccessEvents = true;
+
+                    // see https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/
+                    options.EmitStaticAudienceClaim = true;
+                })
+                .AddInMemoryIdentityResources(Config.IdentityResources)
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddInMemoryClients(Config.Clients)
+                .AddAspNetIdentity<ApplicationUser>();
+
+            return builder.Build();
+        }
+
+        public static WebApplication ConfigurePipeline(this WebApplication app)
+        {
+            app.UseSerilogRequestLogging();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseIdentityServer();
+            app.UseAuthorization();
+
+            app.MapRazorPages()
+                .RequireAuthorization();
+
+            return app;
+        }
+    }
+}
